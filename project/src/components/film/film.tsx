6b58d921d869b6c267c831262473dc35984775a8
Соколов Алексей <av.sok@skbkontur.ru>
2022-10-04 01:31:32 +0500

    feat: main and film components

---
+++ project/src/components/film/film.tsx
@@ -0,0 +1,12 @@
+function Film({src, name}: {src: string, name: string}): JSX.Element {
+    return (
+        <article className="small-film-card catalog__films-card">
+            <div className="small-film-card__image">
+                <img src={src} alt={name} width="280" height="175" />
+            </div>
+            <h3 className="small-film-card__title">
+                <a className="small-film-card__link" href="film-page.html">{name}</a>
+            </h3>
+        </article>
+    );
+}
